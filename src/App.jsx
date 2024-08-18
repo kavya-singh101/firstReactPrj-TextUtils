2e016cc3ab843b8cf2bcb57c3834d4e0c5ee86f5
kavya-singh101 <madhujeet111@gmail.com>
2024-08-18 18:43:35 +0530

    TextUtil first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import './App.css'
+import About from './components/About.jsx';
+import Navbar from './components/Navbar.jsx';
+import TextForm from './components/TextForm.jsx';
+import Alert from './components/Alert.jsx';
+
+import {
+  BrowserRouter as Router, Routes as Switch, Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const [mode, setMode] = useState("light");
+  const [bodyCol, setBodyCol] = useState(null);
+  const [activeSwitch, setActiveSwitch] = useState(null);
+  // if(mode==='light'){
+  //   document.body.style.backgroundColor='#dddada';
+  // }
+  if ((mode === 'dark' || mode === 'light') && bodyCol === null) {
+    document.body.style.backgroundColor = mode === 'dark' ? '#190368' : '#dddada'
+  }
+
+
+  const toggleBody = (col, switchId) => {
+    if (bodyCol === col) {
+      document.body.style.backgroundColor = mode === 'dark' ? '#190368' : '#dddada'
+      setBodyCol(null);
+      setActiveSwitch(null)
+      document.getElementById(switchId).style.backgroundColor = "#ffff";
+      document.getElementById(switchId).style.borderColor = "#ffff";
+    }
+    else {
+      document.body.style.backgroundColor = col;
+      setBodyCol(col);
+      setActiveSwitch(switchId);
+      document.getElementById(switchId).style.backgroundColor = col;
+      document.getElementById(switchId).style.borderColor = col;
+
+    }
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      showAlert("Dark Mode is Enabled", "success")
+      document.title = 'TextUtils - Dark Mode'
+      // document.body.style.backgroundColor='#454545';
+    }
+    else {
+      setMode('light')
+      showAlert("Light Mode is Enabled", "success")
+      document.title = 'TextUtils'
+      // document.body.style.backgroundColor='#dddada';
+    }
+  }
+  return (
+    <>
+      {/* <Navbar title="textUtils prop wala" aboutTitle="About Us"/> */}
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutTitle="About"
+          mode={mode}
+          toggleBody={toggleBody}
+          activeSwitch={activeSwitch}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text" mode={mode} />} />
+          </Switch>
+        </div>
+      </Router>
+
+
+    </>
+  );
+}
+
+export default App
---
+++ src/components/TextForm.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+    const [text, setText]=useState('')
+    // setText("Enter your feedback"); 
+    //correct way to change the value of text (state)
+
+    const upClick=()=>{
+        // console.log("Button is clicked")
+        // setText("click kar dia re chore");
+        setText(text.toUpperCase());
+    }
+    const lowClick=()=>{
+        // console.log("Button is clicked")
+        // setText("click kar dia re chore");
+        setText(text.toLowerCase());
+    }
+    const handleOnChange=(event)=>{
+        // to change the value of value={text} to value={setText}
+        setText(event.target.value);
+    }
+    const copyTxt=()=>{
+        let text=document.getElementById('myBox').value;
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to clipboard","success");
+    }
+    const clearTxt=()=>{
+        // to change the value of value={text} to value={setText}
+        // let text=document.getElementById('myBox');
+        // text.value="";
+        setText("")
+    }
+    const wordCount=()=>{
+        if(text===""){
+            return 0;
+        }
+        if (text.charAt(text.length-1)===" ") {
+            return text.split(" ").length-1
+        }
+        return text.split(" ").length;
+    }
+    const removeSpaces=()=>{
+        setText(text.split(/[ ]+/).join(" "));
+    }
+    return (
+        <>
+        <div className="container">
+            <h1 className={`text-${props.mode==='light'?'dark':'light'}`}>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className={`form-control`} style={{backgroundColor: props.mode==='dark'?'#929090':'white', color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" value={text} rows="12" placeholder='Enter your text here'></textarea>
+            </div>
+            <button className="btn btn-danger btn-outline-primary mx-2 text-dark" onClick={upClick}>Convert to uppercase</button>
+            <button className="btn btn-danger btn-outline-primary mx-2 text-dark" onClick={lowClick}>Convert to lowercase</button>
+            <button className="btn btn-danger btn-outline-primary mx-2 text-dark" onClick={copyTxt}>Copy Text</button>
+            <button className="btn btn-danger btn-outline-primary mx-2 text-dark" onClick={clearTxt}>Clear Text</button>
+            <button className="btn btn-danger btn-outline-primary mx-2 text-dark" onClick={removeSpaces}>Remove Extra spaces</button>
+        </div>
+        <div className={`container my-3 text-${props.mode==='light'?'dark':'light'}`} >
+            <h1>Your text summary</h1>
+            <p>Number of words={wordCount()} and number of characters={text.length}</p>
+            <p>{0.008 * text.split(" ").length} Minuts required to read</p>
+            <h3>Preview</h3>
+            <p>{text.length>0?text:'Your text preview will appear here'}</p>
+        </div>
+        </>
+    )
+}

40a3e3931f6149551c021c048c8622734c24634f
kavya-singh101 <130764087+kavya-singh101@users.noreply.github.com>
2025-02-20 02:08:50 +0530

    Update TextForm.jsx

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -61,7 +61,7 @@ export default function TextForm(props) {
             <p>Number of words={text.split(/\s+/).filter((element)=>{return element.length!==0}).length} and number of characters={text.length}</p>
             {/* <p>Number of words={wordCount()} and number of characters={text.length}</p> */}
             <p>{0.008 * text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minuts required to read</p>
-            <h3>Preview</h3>
+            <h3>Preview:</h3>
             <p>{text.length>0?text:'Your text preview will appear here'}</p>
         </div>
         </>
